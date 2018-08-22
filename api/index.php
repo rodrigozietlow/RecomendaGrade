c7dce849e5a029e5dfc045fc07121e0f633c6c56
Rodrigo Zietlow <rodrigo.zietlow@gmail.com>
2018-08-21 22:08:52 -0300

    raiz da api

--- api/index.php
+++ api/index.php
@@ -1,3 +1,45 @@
 <?php
-print_r($_GET);
+require 'vendor/autoload.php';
+
+$url = $_GET;
+
+// rotas estáticas:: podem ser mudadas no futuro
+$rotas = array(
+	"disciplina" => array( // é um exemplo pra mostrar como funciona o roteamento
+		"Modelo" => "ModeloDisciplina",
+		"Controle" => "ControleDisciplina",
+		"Visao" => "VisaoDisciplina"
+	),
+	"curso" => array(
+		"Modelo" => "ModeloCurso",
+		"Controle" => "ControleCurso",
+		"Visao" => "VisaoCurso"
+	),
+	"index" => array()
+);
+
+$rotasQuebradas = explode("/", $url['rota']);
+
+$verbo = $_SERVER['REQUEST_METHOD'];
+
+
+if(empty($rotasQuebradas)){
+	// enviar erro
+}
+else{
+	$rotaEscolhida = $rotas[$rotasQuebradas[0]] ?? "index";
+	print_r($rotasQuebradas);
+	echo "<br>";
+	print_r($rotaEscolhida);
+	// $modelo = new $rotaEscolhida["Modelo"]();
+	// and so on
+	if($verbo == "GET"){
+		if(count($rotasQuebradas) >= 2) { // tem disciplina/id
+			// executa $modelo->buscar($rotasQuebradas[1])
+		}
+	}
+	else if($verbo == "POST"){
+		// chamar os métodos de salvar e buscar os dados
+	}
+}
 ?>
