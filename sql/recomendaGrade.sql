6076886bcf5b8753ca6710c370511044a5200155
marcelorambomartins <30699523+marcelorambomartins@users.noreply.github.com>
2018-08-22 01:08:31 -0500

    Criada a Tabela de disciplina

--- sql/recomendaGrade.sql
+++ sql/recomendaGrade.sql
@@ -1,1 +1,15 @@
 CREATE DATABASE recomendagrade;
+
+CREATE TABLE disciplina(
+id INTEGER AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(100) NOT NULL, #não pode duplicar
+periodo INTEGER NOT NULL,
+creditos VARCHAR(25) NOT NULL,
+cargaHoraria REAL NOT NULL,
+idCurso INTEGER NOT NULL,
+idRequisito INTEGER,
+dataCadastro DATE NOT NULL,
+FOREIGN KEY (idCurso) REFERENCES curso(id));
+
+INSERT INTO disciplina(nome,periodo,creditos,cargaHoraria,idCurso,idRequisito,dataCadastro)
+	VALUES ('Matematica',1, 4,60,1,1,'2018-08-22');
