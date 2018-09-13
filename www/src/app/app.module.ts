59a271cb605f88e9d183432cb33da5153b9db0bb
Rodrigo Zietlow <rodrigo.zietlow@gmail.com>
2018-09-12 21:00:08 -0300

    Curso validação

--- www/src/app/app.module.ts
+++ www/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
@@ -26,7 +26,8 @@ import { RoutingModule } from './routing.module';
     BrowserModule,
     HttpClientModule,
 	FormsModule,
-	RoutingModule
+	RoutingModule,
+	ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- www/src/app/curso-form/curso-form.component.ts
+++ www/src/app/curso-form/curso-form.component.ts
@@ -1,46 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormControl, Validators, FormGroup } from '@angular/forms'
 
 @Component({
-  selector: 'app-curso-form',
-  templateUrl: './curso-form.component.html',
-  styleUrls: ['./curso-form.component.css']
+	selector: 'app-curso-form',
+	templateUrl: './curso-form.component.html',
+	styleUrls: ['./curso-form.component.css']
 })
 export class CursoFormComponent implements OnInit {
 
-	public objeto = {
-  	  "id" : "",
-      "nomeCurso" : "",
-      "nomePeriodos" : "",
-      "qtPeriodos" : "",
-      "cargaMinima" : "",
-      "idCoordenador" : "",
-      "publico" : false,
-      "dataCadastro" : "",
-      "disciplinas" : []
-    }
+	public curso = {
+		"id" : "",
+		"nomeCurso" : "",
+		"nomePeriodos" : "",
+		"qtPeriodos" : "",
+		"cargaMinima" : "",
+		"idCoordenador" : "",
+		"publico" : false,
+		"dataCadastro" : "",
+		"disciplinas" : []
+	};
 
-    constructor(private http: HttpClient) { }
+	public form:FormGroup = new FormGroup({
+		nomeCurso: new FormControl({value:this.curso.nomeCurso, disabled:"true"}),
+		nomePeriodos: new FormControl(this.curso.nomePeriodos, [Validators.required, Validators.maxLength(25)]),
+		qtPeriodos: new FormControl(this.curso.qtPeriodos, [Validators.max(127), Validators.min(1), Validators.required]),
+		cargaMinima: new FormControl(this.curso.cargaMinima, [Validators.required, Validators.min(0), Validators.max(999999.99)]),
+		publico: new FormControl({value:this.curso.disciplinas.length, disabled: (this.curso.disciplinas.length == 0) ? "true" : "false"}),
+	});
 
-    ngOnInit() {
-  	  this.buscarCurso();
-    }
+	//public qtPeriodosControl:any = new FormControl(this.curso.qtPeriodos, [Validators.max(127), Validators.min(1), Validators.required]);
 
-    buscarCurso(){
-      this.http.get<any>("http://192.168.103.223/ads_desenv/ads_dev/api/curso/1").subscribe((dados) => {
-        this.objeto = dados;
-      });
-    }
+	constructor(private http: HttpClient) { }
 
-    salvarCurso(){
-      const opcoes = {
-        headers: new HttpHeaders({
-          'Content-Type':  'application/json',
-        })
-      };
-      this.http.put<any>("http://192.168.103.223/ads_desenv/ads_dev/api/curso/1", this.objeto, opcoes).subscribe((dados) => {
-        console.log(dados);
-      });
-    }
+	ngOnInit() {
+		this.buscarCurso();
+	}
+
+	buscarCurso(){
+		this.http.get<any>("http://192.168.103.223/ads_desenv/ads_dev/api/curso/1").subscribe((dados) => {
+			console.log(dados)
+			this.curso = dados;
+		});
+	}
+
+	salvarCurso(){
+
+
+
+		const opcoes = {
+			headers: new HttpHeaders({
+				'Content-Type':  'application/json',
+			})
+		};
+		this.http.put<any>("http://192.168.103.223/ads_desenv/ads_dev/api/curso/1", this.curso, opcoes).subscribe((dados) => {
+			alert("Salvo com sucesso");
+			console.log(dados);
+		});
+	}
 
 }
