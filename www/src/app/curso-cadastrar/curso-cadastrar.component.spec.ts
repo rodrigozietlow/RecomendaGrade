264e2304a1ede0355e3c45e5baa4e630deed0ed6
luciana.zimmer <luciana.zimmer@FELIZ.INTERNO>
2018-12-12 22:49:35 -0200

    front curso + usuario

---
+++ www/src/app/curso-cadastrar/curso-cadastrar.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CursoCadastrarComponent } from './curso-cadastrar.component';
+
+describe('CursoCadastrarComponent', () => {
+  let component: CursoCadastrarComponent;
+  let fixture: ComponentFixture<CursoCadastrarComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CursoCadastrarComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CursoCadastrarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- www/src/app/routing.module.ts
+++ www/src/app/routing.module.ts
@@ -8,12 +8,14 @@ import { DisciplinaEditarComponent } from './disciplina-editar/disciplina-editar
 import { DisciplinaCadastrarComponent } from './disciplina-cadastrar/disciplina-cadastrar.component';
 import { LoginComponent } from './login/login.component';
 import { AlunoEditarComponent } from './aluno-editar/aluno-editar.component';
+import { CursoCadastrarComponent } from './curso-cadastrar/curso-cadastrar.component';
 
 const rotas : Routes = [
 	/* raiz e extras */
 	{path: '', redirectTo: '/curso/disciplinas', pathMatch: 'full' },
 	{path: 'index', component: IndexComponent},
 	/* curso */
+	{path: 'curso/cadastrar', component: CursoCadastrarComponent},
 	{path: 'curso/editar', component: CursoFormComponent},
 	{path: 'curso/disciplinas', component: CursoDisciplinasComponent},
 	/* disciplina */
